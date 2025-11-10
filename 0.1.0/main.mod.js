7662fec3f0fbb17dd35ed8d72cfdd0a23375913c
Orangy <orangywastaken@gmail.com>
2025-11-10 15:45:51 -0500

    pml api setup

---
+++ 0.1.0/main.mod.js
@@ -0,0 +1,7 @@
+import { PolyMod, MixinType} from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js"
+
+class PmlApi extends PolyMod {
+    
+}
+
+export let polyMod = new PmlApi();
